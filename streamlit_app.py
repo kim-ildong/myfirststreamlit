8aaf7fbe29fd432a07189699d67f5d2a1605a76a
kim-ildong <kimid7@naver.com>
2024-10-23 10:03:14 +0000

    앱만들기 시작

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("👋🏻 Streamlit 앱 만들기")
-st.subheader("이 페이지는 실습 페이지입니다.")
-st.write("오른쪽 위의 'fork' 버튼을 눌러주세요. 이 페이지와 앱이 그대로 복사됩니다.")
+st.title("👋🏻 연수 실습 페이지(1023)")
+st.subheader("이 페이지는 실습 페이지입니다. by KIM ILDONG")
+st.info("서울반도체고등학교 김일동입니다.")
 
-st.link_button("streamlit 매뉴얼 페이지 바로가기!", "https://surish.notion.site/streamlit-113eef51495c8083986cc65f2d07470c?pvs=73")
+st.link_button("김일동 기터브 페이지 바로가기!", "https://github.com/kim-ildong/myfirststreamlit")
 
 # st.success("초록색 창")
 # st.error("빨간색 창")
-st.info("파란색 창")
+# st.info("파란색 창")
 # st.warning("노란색 창") # ctrl+/ : 주석처리
-st.image("https://media.giphy.com/media/8bE0EERrvXkq5S9BCa/giphy.gif?cid=ecf05e479tvvde0o3d3do3g67yoq0jn4zzyg9hyt63stuf0x&ep=v1_gifs_related&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
+st.image("https://media.giphy.com/media/ua7vVw9awZKWwLSYpW/giphy.gif?cid=790b761108nfakcxlzw5dnp42pg26lngbkiy36np7mrtwrpg&ep=v1_gifs_search&rid=giphy.gif&ct=g") 
+st.video("https://www.youtube.com/watch?v=XGVNDc9Zip4&pp=ygUG7J2M7JWF")
